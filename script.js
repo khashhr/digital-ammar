1b4f96abc9c6d635a02c41e6bd6e7946caacdd91
khashhr <yutaydh@gmail.com>
2024-09-05 15:05:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateTime() {
     const timeElement = document.getElementById('time');
-    const now = new date();
+    const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
