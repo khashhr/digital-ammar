5cb3eb9f1e1fe9e53c8af01451800f5f8afe1dcf
khashhr <yutaydh@gmail.com>
2024-09-05 14:58:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function updateTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = STring(now.getSeconds()).padStart(2, '0');
-    timeElement.textContent = '${hours}:${minutes}:${seconds}';
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
 setInterval(updateTime, 1000);
